bfef13bccd26e6a79b6c8809929bc7217e8a95b8
ASHUTOSH VAISHNAV <ashutosh.16jccs032@jietjodhpur.ac.in>
2021-01-20 12:55:27 +0530

    Add QProfile(CWoD)

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Ashutosh Vaishnav",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Ashutosh Vaishnav and I'm a Software Engineer!",
+  githubUrl: "https://github.com/avaishnav6292",
+  linkedinUrl: "https://linkedin.com/in/avaishnav6292",
+  cvLink: "https://avaishnav6292.github.io/assets/docs/Ashutosh_Vaishnav_Resume.pdf",
+};
